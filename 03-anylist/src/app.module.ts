ccfa799876efcfbec7d5ee9eed210084fc8a601a
Mauricio <Siumauricio@unitec.edu>
2022-11-14 16:00:39 -0600

    feat: add items module

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -4,8 +4,15 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../ormconfig';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
@@ -13,6 +20,7 @@ import { ItemsModule } from './items/items.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault],
     }),
+
     ItemsModule,
   ],
   controllers: [],
